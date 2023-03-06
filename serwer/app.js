d0ef26232f9df1bcd3c139a441765caa05597006
Z4joo <franko.galazka@gmail.com>
2023-03-06 11:20:23 +0100

    Update app.js

--- serwer/app.js
+++ serwer/app.js
@@ -6,27 +6,24 @@ const app = express()
 
 app.use(cors())
 
-const port = 3002
+ const port = 3005
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "localhost" ,
+    user: "root" ,
+    password: "" ,
     database: "zaj_21"
 })
 
-con.connect(function(err){
-    if(err){ 
-    console.log(err)
-    }
-    else console.log("polaczono z baza danych")
+con.connect(function (err){
+    if(err) console.log(err)
+
+    else console.log("Polaczono z baza danych")
 })
 
-
-app.get("/",(req, res)=>{
-    res.send("ok")
+app.get("/", (req, res)=>{
+    res.send("OK")
 })
-
 
 app.get("/select", (req, res)=>{
     const sql = "SELECT * FROM test"
@@ -38,25 +35,19 @@ app.get("/select", (req, res)=>{
     })
 }) 
 
+app.get("/add/:imie/:nazwisko/:klasa", (req, res)=>{ 
+    const imie = req.params.imie
+    const nazwisko = req.params.nazwisko
+    const klasa = req.params.klasa
 
-app.get("/add/:Imie/:Nazwisko/:Klasa", (req, res)=> {
-    const imie = req.params.Imie
-    const nazwisko = req.params.Nazwisko
-    const klasa = req.params.Klasa
 
-   
-   
     const sql = `INSERT INTO test (Imie,Nazwisko,Klasa) VALUES ('${imie}' , '${nazwisko}' , '${klasa}')` 
     con.query(sql, function(err, result, fileds){
         if(err) console.log(err)
 
         else res.send("dodano rekord")
     })
-
-
 })
-
-
 app.listen(port, ()=>{
-    console.log("aplikacja dziala na porcie " + port)
-}) 
+    console.log("aplikacja dziala na porcie" + port)
+})
