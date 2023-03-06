cd5bd3d6239b239619fca9052ce58eb9907cdae9
Z4joo <franko.galazka@gmail.com>
2023-03-06 09:40:19 +0100

    Update app.js

--- serwer/app.js
+++ serwer/app.js
@@ -8,6 +8,21 @@ app.use(cors())
 
 const port = 3002
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "zaj_21"
+})
+
+con.connect(function(err){
+    if(err){ 
+    console.log(err)
+    }
+    else console.log("polaczono z baza danych")
+})
+
+
 app.get("/",(req, res)=>{
     res.send("ok")
 })
